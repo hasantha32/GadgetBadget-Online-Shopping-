e35989073af2ce1ed840f10c051650e97faf14d3
hasantha32 <hasanthamadushan32@gmail.com>
2021-06-10 17:54:39 +0530

    API file implemented

---
+++ GadgetBadget/src/com/RegistryAPI.java
@@ -0,0 +1,103 @@
+package com;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistryAPI
+ */
+@WebServlet("/RegistryAPI")
+public class RegistryAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	Registry regObj = new Registry();
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) 
+	{ 
+		Map<String, String> map = new HashMap<String, String>(); 
+		try
+		{ 
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+				String queryString = scanner.hasNext() ? 
+						scanner.useDelimiter("\\A").next() : ""; 
+				scanner.close(); 
+	 
+				String[] params = queryString.split("&"); 
+				for (String param : params) 
+				{
+					String[] p = param.split("=");
+					map.put(p[0], p[1]);
+				} 
+		} 
+		catch (Exception e) 
+	 { 
+	 } 
+		return map; 
+	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistryAPI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String output = regObj.insertItem(request.getParameter("firstName"), 
+				 request.getParameter("lastName"), 
+				 request.getParameter("email"), 
+				 request.getParameter("password"));
+		
+			response.getWriter().write(output); 
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		Map paras = getParasMap(request);
+		
+		String output = regObj.updateItem(paras.get("hidItemIDSave").toString(), 
+							paras.get("firstName").toString(), 
+							paras.get("lastName").toString(), 
+							paras.get("email").toString(), 
+							paras.get("password").toString());
+		
+		response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map paras = getParasMap(request); 
+		
+		String output = regObj.deleteItem(paras.get("accountID").toString()); 
+		
+		response.getWriter().write(output);
+	}
+
+}
